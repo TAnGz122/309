e607eeb05df61d7f24cf9bf3c95da5b27c3c7b7e
TAnGz122 <pddaimond@gmail.com>
2025-09-14 20:08:41 +0700

    connect.php

--- connect.php
+++ connect.php
@@ -1,18 +1,15 @@
 <?php
-// กำหนดค่าตัวแปรสำหรับการเชื่อมต่อฐานข้อมูล
-$servername = "localhost"; // หรือ IP Address ของ Server
-$username = "root";       // ชื่อผู้ใช้งานฐานข้อมูล
-$password = "";           // รหัสผ่าน (ถ้ามี)
-$dbname = "classicmodels"; // ชื่อฐานข้อมูลจากไฟล์ .sql ของคุณ
+$servername = "localhost"; 
+$username = "root";       
+$password = "";           
+$dbname = "classicmodels"; 
 
-// สร้างการเชื่อมต่อด้วย MySQLi
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// ตรวจสอบว่าการเชื่อมต่อสำเร็จหรือไม่
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// ตั้งค่าชุดอักขระ (Charset) เป็น UTF-8 เพื่อให้รองรับภาษาไทย
 $conn->set_charset("utf8");
+
 ?>
